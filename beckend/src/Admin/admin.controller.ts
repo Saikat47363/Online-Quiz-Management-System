296b73e298f9b2a82ba0a2c4895f1a34354f980f
Saikat47363 <mdfirozmahmudsaikat@gmail.com>
2025-07-28 12:39:10 +0600

    Saikat_Valitation

--- beckend/src/Admin/admin.controller.ts
+++ beckend/src/Admin/admin.controller.ts
@@ -1,35 +1,48 @@
-import { Body, Controller, Delete, Get, Param, Post, Put, Query } from "@nestjs/common";
-import { AdminService } from "./admin.service";
-
-@Controller("admin")
-export class AdminController{
-    constructor(private readonly adminService:AdminService)
-    {}
-
-    @Get('all')
-    getAdmin(){
-        return this.adminService.getAdmin();
-    }
-    getAdminbyNameandID(@Query('name') name:string ,@Query('id') id:number): object {
-return this.adminService.getAdminByNameandID(name,id);
-    }
-
-    @Post("addadmin")
-   addAdmin(@Body() admindata: object)  : object{
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Put,
+  Query,
+} from '@nestjs/common';
+import { AdminService } from './admin.service';
+import { CreateAdminDto } from './dto/admin.dto';
+ 
+@Controller('admin')
+export class AdminController {
+  constructor(private readonly adminService: AdminService) {}
+ 
+  @Get('all')
+  getAdmin() {
+    return this.adminService.getAdmin();
+  }
+ 
+  @Get()
+  getAdminByNameAndID(
+    @Query('name') name: string,
+    @Query('id') id: number,
+  ): object {
+    return this.adminService.getAdminByNameandID(name, id);
+  }
+ 
+  @Post('addadmin')
+  addAdmin(@Body() admindata: CreateAdminDto): object {
     return this.adminService.addAdmin(admindata);
-   }
-   @Delete('delete/:id')
-    deleteAdmin(@Param('id') id: number): String {
-        return this.adminService.deleteAdmin(id);
-    }
-    
-    @Delete('deleteAll')
-    declareAllAdmins(@Param('id')id :number): String {
-        return "checking delete all admins";
-    }
-
-    @Put('edit/:id')
-    editAdmin(@Param('id') id: number, @Body() updatedData: object): String {
-        return this.adminService.editAdmin(id, updatedData);
-    }
+  }
+ 
+  @Delete('delete/:id')
+  deleteAdmin(@Param('id') id: number): string {
+    return this.adminService.deleteAdmin(id);
+  }
+ 
+  @Put('edit/:id')
+  editAdmin(
+    @Param('id') id: number,
+    @Body() updatedData: CreateAdminDto,
+  ): string {
+    return this.adminService.editAdmin(id, updatedData);
+  }
 }
--- beckend/src/app.module.ts
+++ beckend/src/app.module.ts
@@ -1,12 +1,7 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { teachersModule } from './Teachers/teachers.module'; 
-
-@Module({
-  imports: [teachersModule],
 import { AdminModule } from './Admin/admin.module';
-
 @Module({
   imports: [AdminModule],
   controllers: [AppController],
